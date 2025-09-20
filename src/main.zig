07b8bd130c727a7841603dfba2394a48b54ddf31
Xavier Shay <contact@xaviershay.com>
2025-09-20 11:11:23 +1000

    collisions and game halt on finish

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,69 @@
 const std = @import("std");
+const expect = std.testing.expect;
 
-const GameSpec = struct { width: u8, height: u8 };
+const GameSpec = struct { width: u8, height: u8, dimensions: Coordinate };
 const Block = enum { none, garbage, z, s, j, l, t, o, i };
 const Direction = enum { north, east, south, west };
-const Coordinate = struct { x: i8, y: i8 };
+const Coordinate = struct {
+    x: i8,
+    y: i8,
+    fn add(self: *const Coordinate, other: Coordinate) Coordinate {
+        return Coordinate{
+            .x = self.x + other.x,
+            .y = self.y + other.y,
+        };
+    }
+
+    fn inBounds(self: *const Coordinate, topLeft: Coordinate, bottomRight: Coordinate) bool {
+        return (self.x >= topLeft.x and self.y >= topLeft.y and self.x < bottomRight.x and self.y < bottomRight.y);
+    }
+};
+
+test "coordinate.inBounds()" {
+    const x = Coordinate{ .x = 0, .y = 10 };
+    try expect(x.inBounds(.{ .x = 0, .y = 10 }, .{ .x = 1, .y = 11 }));
+    try expect(!x.inBounds(.{ .x = 0, .y = 10 }, .{ .x = 0, .y = 11 }));
+    try expect(!x.inBounds(.{ .x = 0, .y = 10 }, .{ .x = 1, .y = 10 }));
+    try expect(!x.inBounds(.{ .x = 1, .y = 10 }, .{ .x = 1, .y = 11 }));
+    try expect(!x.inBounds(.{ .x = 0, .y = 11 }, .{ .x = 1, .y = 11 }));
+    try expect(!x.inBounds(.{ .x = 1, .y = 11 }, .{ .x = 1, .y = 11 }));
+}
+
 const Tetromino = struct { pattern: [4]Coordinate, block: Block };
+
+const GameState = enum { running, halted };
 
 const Game = struct {
     spec: GameSpec,
+    state: GameState,
     playfield: []Block,
     current: ?struct { position: Coordinate, orientation: Direction, tetromino: Tetromino },
+
+    fn at(self: *Game, coordinate: Coordinate) Block {
+        if (coordinate.inBounds(Coordinate{ .x = 0, .y = 0 }, self.spec.dimensions)) {
+            std.debug.print("{d}x{d} ", .{ coordinate.x, coordinate.y });
+            const idx = @as(u8, @intCast(coordinate.y)) * self.spec.width + @as(u8, @intCast(coordinate.x));
+            return self.playfield[idx];
+        } else {
+            return Block.none;
+        }
+    }
+
+    fn setAt(self: *Game, coordinate: Coordinate, block: Block) void {
+        if (coordinate.inBounds(Coordinate{ .x = 0, .y = 0 }, self.spec.dimensions)) {
+            const idx = @as(u8, @intCast(coordinate.y)) * self.spec.width + @as(u8, @intCast(coordinate.x));
+            self.playfield[idx] = block;
+        }
+    }
 };
 
 const t = Tetromino{
     .block = Block.t,
     .pattern = [4]Coordinate{
+        Coordinate{ .x = 0, .y = -1 },
+        Coordinate{ .x = -1, .y = 0 },
         Coordinate{ .x = 0, .y = 0 },
-        Coordinate{ .x = -1, .y = 1 },
-        Coordinate{ .x = 0, .y = 1 },
-        Coordinate{ .x = 1, .y = 1 },
+        Coordinate{ .x = 1, .y = 0 },
     },
 };
 
@@ -30,16 +75,14 @@ pub fn main() void {
 
 fn run() error{OutOfMemory}!void {
     //var rng = SimpleRNG.init(12345);
-    const spec = GameSpec{
-        .width = 10,
-        .height = 20,
-    };
+    const spec = GameSpec{ .width = 10, .height = 10, .dimensions = .{ .x = 10, .y = 10 } };
 
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
     var game = Game{
         .spec = spec,
+        .state = GameState.running,
         .playfield = try allocator.alloc(Block, spec.width * spec.height),
         .current = null,
         // .{
@@ -50,14 +93,14 @@ fn run() error{OutOfMemory}!void {
     };
     @memset(game.playfield, Block.none);
 
-    while (true) {
+    while (game.state == GameState.running) {
         // Not in render so that we can dump debug stuff in update()
         const stdout = std.fs.File.stdout();
         _ = stdout.write("\x1b[2J\x1b[H") catch 0;
 
         update(&game);
         try render(game);
-        std.Thread.sleep(1_000_000_00);
+        std.Thread.sleep(1_000_000_00 / 3);
     }
 }
 
@@ -66,13 +109,42 @@ fn update(game: *Game) void {
         game.current = .{
             .tetromino = t,
             .orientation = Direction.north,
-            .position = Coordinate{ .x = 5, .y = 0 },
+            .position = Coordinate{ .x = 5, .y = -1 },
         };
     }
     var p = &game.current.?.position;
-    // TODO: Proper collision detection
-    if (p.y < game.spec.height - 3) {
-        p.y += 1;
+    p.y += 1;
+
+    const currentG = game.current.?;
+    const currentT = currentG.tetromino;
+    var blocked = false;
+
+    for (currentT.pattern) |offset| {
+        const location = p.add(offset);
+
+        if (game.at(location) != Block.none or !location.inBounds(.{ .x = 0, .y = -1 }, game.spec.dimensions)) {
+            p.y -= 1;
+            blocked = true;
+            // TODO: this should be < not <= and there's a flashing thing that happens at the end, so a bug somewhere.
+            if (p.y <= 0) {
+                game.state = GameState.halted;
+                game.current = null;
+            }
+            break;
+        }
+    }
+
+    if (blocked and game.state == GameState.running) {
+        for (currentT.pattern) |offset| {
+            const location = p.add(offset);
+
+            game.setAt(location, currentT.block);
+        }
+        game.current = .{
+            .tetromino = t,
+            .orientation = Direction.north,
+            .position = Coordinate{ .x = 5, .y = 0 },
+        };
     }
 }
 
@@ -88,11 +160,10 @@ fn render(game: Game) error{OutOfMemory}!void {
     if (game.current != null) {
         const current = game.current.?;
         // TODO: rotation
-        for (0..4) |i| {
-            const offset = current.tetromino.pattern[i];
-            const location = Coordinate{ .x = current.position.x + offset.x, .y = current.position.y + offset.y };
+        for (current.tetromino.pattern) |offset| {
+            const location = current.position.add(offset);
 
-            if (location.x >= 0 and location.x < game.spec.width and location.y >= 0 and location.y < game.spec.height) {
+            if (location.inBounds(.{ .x = 0, .y = 0 }, game.spec.dimensions)) {
                 playfieldRender[@as(u8, @intCast(location.y)) * spec.width + @as(u8, @intCast(location.x))] = current.tetromino.block;
             }
         }
@@ -101,9 +172,9 @@ fn render(game: Game) error{OutOfMemory}!void {
     // Print playfield contents
     const playfield_slice = playfieldRender[0 .. spec.width * spec.height];
 
-    std.debug.print("\n\n\n----------", .{});
-    for (0..spec.height - 1) |y| {
-        for (0..spec.width - 1) |x| {
+    std.debug.print("\n\n\n----------\n", .{});
+    for (0..spec.height) |y| {
+        for (0..spec.width) |x| {
             const idx = y * spec.width + x;
             const block = playfield_slice[idx];
             switch (block) {
@@ -117,7 +188,7 @@ fn render(game: Game) error{OutOfMemory}!void {
         }
         std.debug.print("\n", .{});
     }
-    std.debug.print("==========", .{});
+    std.debug.print("==========\n", .{});
 }
 
 // Simple Linear Congruential Generator (LCG)
