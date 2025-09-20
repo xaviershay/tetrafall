16a7e9068201e21580353f9c9a18f51692ca7b28
Xavier Shay <contact@xaviershay.com>
2025-09-20 14:42:57 +1000

    start recording some setup steps

---
+++ bin/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Needed for notcurses
+# https://github.com/dankamongmen/notcurses/blob/master/INSTALL.md
+sudo apt-get install build-essential cmake doctest-dev libavdevice-dev libdeflate-dev libgpm-dev libncurses-dev libqrcodegen-dev libswscale-dev libunistring-dev pandoc pkg-config
